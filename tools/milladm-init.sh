15db6002473ecfa1ab1d1493d699766158fa9ee2
Trent Mick <trentm@gmail.com>
2013-11-13 11:44:20 -0800

    milladm-init fledgling 'milladm' tool. 'init' here sets up the mill dir in manta (i.e. assets)

---
+++ tools/milladm-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Initialize the Mill dir ($MILL_DIR or /$MANTA_USER/stor/mill)
+# layout (e.g. assets, base dirs) in Manta.
+#
+# TODO: should this be 'milld init'? Only really want *one* coordinating
+# admin to init that -- not each milld agent. Perhaps eventually a
+# milladm init.
+#
+
+set -o errexit
+TOP=$(cd $(dirname $0)/../; pwd)
+
+
+if [[ -z "$MILL_DIR" ]]; then
+    MILL_DIR=/$MANTA_USER/stor/mill
+fi
+echo "Initializing Mill dir ($MILL_DIR) in Manta"
+
+mmkdir $MILL_DIR/logs
+mmkdir $MILL_DIR/assets
+mput -f $TOP/assets/tlog.js $MILL_DIR/assets/tlog.js
